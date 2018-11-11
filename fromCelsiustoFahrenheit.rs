3b6a20c643e7dd0a16ed7a4abd0f5db49a48fa1f
RinatValiullov <4rontender@gmail.com>
2018-11-11 11:01:58 +0500

    added a simple temperature converter

---
+++ fromCelsiustoFahrenheit.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let cel = 15;
+
+    let far = (cel * 9 / 5) + 32;
+
+    println!(
+        "A temperature in Celsius: {0} degrees  is equal to {1} degrees in Fahrenheit",
+        cel, far
+    );
+}
